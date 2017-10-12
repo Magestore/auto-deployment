80c1f95d1c58afb74b978b9e1154132cd9d5b4f8
Alex <alex@magestore.com>
2017-10-12 15:26:33 +0700

    update git cache password

--- demo-magento2.magestore.com/starter-01/pull/pos.sh
+++ demo-magento2.magestore.com/starter-01/pull/pos.sh
@@ -3,6 +3,7 @@ if [ -d "$DIRECTORY" ]; then
     cd "$DIRECTORY"
     git pull
 else
+    git config --global credential.helper 'cache --timeout=99999999999'
     git clone -b Omnichannel https://github.com/Magestore/WebPOS-Magento2-New "$DIRECTORY"
     cd "$DIRECTORY"
 fi
