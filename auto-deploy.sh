5d7bb0a505b482a03ab5fb449e9a3e0cf4c2980d
Alex <alex@magestore.com>
2017-10-12 14:56:32 +0700

    update auto-deploy

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,6 +1,7 @@
 AUTO_DEPLOY="auto-deploy"
-if [ -d "$AUTO_DEPLOY" ]; then
-    git clone -b master https://github.com/Magestore/WebPOS-Magento2-New "$AUTO_DEPLOY"
+if [ -d "$AUTO_DEPLOY/.git" ]; then
+    rm -rf $AUTO_DEPLOY
+    git clone -b master https://github.com/Magestore/auto-deployment "$AUTO_DEPLOY"
 else
     cd "$AUTO_DEPLOY"
     git pull
