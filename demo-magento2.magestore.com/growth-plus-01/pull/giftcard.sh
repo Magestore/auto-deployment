48fdc3e1cdb92136b5c4b883868584f077fd55a3
Alex <alex@magestore.com>
2017-10-13 13:50:26 +0700

    update giftcard

--- demo-magento2.magestore.com/growth-plus-01/pull/giftcard.sh
+++ demo-magento2.magestore.com/growth-plus-01/pull/giftcard.sh
@@ -1,6 +1,7 @@
 DO_DEPLOY=0
 REPO="Giftcard-Magento2"
 DIRECTORY=$REPO
+rm -rf "$DIRECTORY"
 if [ -d "$DIRECTORY" ]; then
     cd "$DIRECTORY"
     STATUS=`git pull`
@@ -9,7 +10,7 @@ if [ -d "$DIRECTORY" ]; then
     fi
 else
     git config --global credential.helper 'cache --timeout=99999999999'
-    git clone -b master https://github.com/Magestore/"$REPO" "$DIRECTORY"
+    git clone -b omnichannel https://github.com/Magestore/"$REPO" "$DIRECTORY"
     cd "$DIRECTORY"
     DO_DEPLOY=1
 fi
