5fd8687e022be1bc15cbb87a70e2dc9e2db802fe
Alex Nguyen <alex@magestore.com>
2017-11-14 14:46:29 +0700

    Create growth-m2.sh

---
+++ package/growth-m2.sh
@@ -0,0 +1,49 @@
+rm -rf package
+rm -rf source
+mkdir package
+
+git config --global credential.helper 'cache --timeout=99999999999'
+
+rm -rf source
+git clone -b dev https://github.com/Magestore/OS-Inventorysuccess-M2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/BarcodeSuccess-M2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b dev https://github.com/Magestore/WebPOS-Magento2-New source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/PurchaseOrderSuccess-M2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/Giftcard-Magento2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/Rewardpoints-Standard-Magento2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/Storecredit-Magento2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/Storepickup-Magento2 source
+cp -R source/* ./package
+
+rm -rf source
+git clone -b master https://github.com/Magestore/SupplierSuccess source
+rm -rf package/app/code/Magestore/SupplierSuccess
+mkdir package/app/code/Magestore/SupplierSuccess
+cp -R source/* ./package/app/code/Magestore/SupplierSuccess
+
+cd package
+rm -rf dev
+rm -rf .git
+rm -f README.md
+
