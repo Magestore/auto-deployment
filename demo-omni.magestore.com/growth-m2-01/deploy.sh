3a57e286a887c8c76b58f49bcb0b215a3e35025b
Alex <alex@magestore.com>
2017-11-21 13:34:38 +0700

    update auto deploy

--- demo-omni.magestore.com/growth-m2-01/deploy.sh
+++ demo-omni.magestore.com/growth-m2-01/deploy.sh
@@ -2,7 +2,7 @@ SERVER_ROOT="/var/www"
 MAGENTO_ROOT="/var/www/m2/growth/01"
 DEPLOY_PATH="demo-omni.magestore.com/growth-m2-01"
 PACKAGE="packages"
-AUTO_DEPLOY="auto-deploy"
+AUTO_DEPLOY="auto-deployment"
 DO_DEPLOY=0;
 
 cd "$SERVER_ROOT"
