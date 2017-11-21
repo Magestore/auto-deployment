e4636952bb3de685c9b7440c779829e8f00bc83b
Alex <alex@magestore.com>
2017-11-21 15:52:26 +0700

    add demo-omni-1.magestore.com

---
+++ demo-omni-1.magestore.com/m2-01/deploy.sh
@@ -0,0 +1,56 @@
+SERVER_ROOT="/var/www"
+MAGENTO_ROOT="/var/www/m2/growth-plus/01"
+DEPLOY_PATH="demo-omni.magestore.com/m2-growth-plus-01"
+PACKAGE="packages"
+AUTO_DEPLOY="auto-deployment"
+DO_DEPLOY=0;
+
+cd "$SERVER_ROOT"
+sh "$AUTO_DEPLOY"/auto-deploy.sh
+
+cp -R "$AUTO_DEPLOY"/"$DEPLOY_PATH" "$MAGENTO_ROOT"/"$AUTO_DEPLOY"
+
+cd "$MAGENTO_ROOT"
+
+if [ -d "$PACKAGE" ]; then
+    cd ./
+else
+    mkdir "$PACKAGE"
+fi
+
+cd "$PACKAGE"
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/barcode.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/giftcard.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/im.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/po.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/pos.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/reward.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/storecredit.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/storepickup.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+SUB_DEPLOY=`sh ../"$AUTO_DEPLOY"/pull/supplier.sh`
+DO_DEPLOY=`[ "$SUB_DEPLOY" = "1" ] && echo "1" || echo $DO_DEPLOY`
+
+
+if [ "$DO_DEPLOY" = "1" ]; then
+    cd ../
+    php bin/magento setup:upgrade
+    php bin/magento cache:clean
+    chmod -R 777 var pub generated
+fi
---
+++ demo-omni-1.magestore.com/m2-01/pull/ordersuccess.sh
@@ -0,0 +1,26 @@
+DO_DEPLOY=0
+REPO="OrderSuccess"
+DIRECTORY=$REPO
+if [ -d "$DIRECTORY" ]; then
+    cd "$DIRECTORY"
+    STATUS=`git pull`
+    if [ "$STATUS" != "Already up-to-date." ]; then
+        DO_DEPLOY=1
+    fi
+else
+    git config --global credential.helper 'cache --timeout=99999999999'
+    git clone -b master https://github.com/Magestore/"$REPO" "$DIRECTORY"
+    cd "$DIRECTORY"
+    DO_DEPLOY=1
+fi
+
+chown -R www-data:www-data *
+
+if [ "$DO_DEPLOY" = "1" ]; then
+    chown -R www-data:ftpuser *
+    rm -rf ../../app/code/Magestore/OrderSuccess
+    mkdir ../../app/code/Magestore/OrderSuccess
+    cp -R -p * ../../app/code/Magestore/OrderSuccess
+fi
+
+echo $DO_DEPLOY
